4cb809ea8994553db47ba21d011121fe22dcb68a
sarvesh kumar <sarveshmaurya306@gmail.com>
2020-10-16 02:37:37 +0530

    x

--- front-end/src/components/Chat.jsx
+++ front-end/src/components/Chat.jsx
@@ -16,40 +16,38 @@ const socket = io.connect("http://localhost:4000");
 
 const BootstrapInput = withStyles((theme) => ({
   root: {
-    "label + &": {
+    'label + &': {
       marginTop: theme.spacing(3),
     },
-    message: {},
   },
   input: {
     borderRadius: 4,
-    position: "relative",
+    position: 'relative',
     backgroundColor: theme.palette.background.paper,
-    border: "1px solid #ced4da",
+    border: '1px solid #ced4da',
     fontSize: 16,
-    padding: "10px 26px 10px 12px",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
     // Use the system font instead of the default Roboto font.
     fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
+      '-apple-system',
+      'BlinkMacSystemFont',
       '"Segoe UI"',
-      "Roboto",
+      'Roboto',
       '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
+      'Arial',
+      'sans-serif',
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
+    ].join(','),
+    '&:focus': {
       borderRadius: 4,
-      borderColor: "#80bdff",
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
     },
   },
 }))(InputBase);
-
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
