8e42b996444cb1d9b45b355211ddba8bd951eb19
sarvesh kumar <58571739+sarveshmaurya306@users.noreply.github.com>
2020-11-28 22:44:50 +0530

    Create dev.js

---
+++ server/utils/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGO_KEY:mongodb://127.0.0.1:27017/kiet-talks-app,
+    AUTH_KEY: "mini_project_1234_abcd"
+};
+
+// "mongodb+srv://sarvesh:radhika8@kiettalkswebapp.iti87.mongodb.net/mini-project?retryWrites=true&w=majority",
+
+
+// 
